4b8ccbcb269982920d011fbd1dac27886125d4c2
Mohammad <msharifi@cafebazaar.ir>
2020-04-10 21:15:36 +0430

    refactor: move index style to stylesheets folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './components/App/App';
+
+import './stylesheets/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
