14029825990be627fd6a7fd2872e2d5258940d79
Mohammad <msharifi@cafebazaar.ir>
2020-04-10 22:50:19 +0430

    feat: add sass-resources-loader to craco

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,12 @@
+const sassResourcesLoader = require('craco-sass-resources-loader');
+
 module.exports = {
-  
+  plugins: [
+    {
+      plugin: sassResourcesLoader,
+      options: {
+        resources: ['./src/stylesheets/variables/_sizes.scss']
+      }
+    }
+  ]
 }
