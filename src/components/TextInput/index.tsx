040c843e3f2db42bce0d52fe6cc3dbc7b6ae22d8
Mohammad <msharifi@cafebazaar.ir>
2020-04-18 19:52:59 +0430

    feat: add TextInput component

---
+++ src/components/TextInput/index.tsx
@@ -0,0 +1,29 @@
+import React, { ChangeEvent } from "react";
+import "./index.scss";
+
+interface TextInputProps {
+  placeholder: string;
+  value: string;
+  onTextChange: (value: string) => void;
+}
+
+export default function TextInput({
+  placeholder,
+  value,
+  onTextChange,
+}: TextInputProps) {
+  const changeInputHandler = (event: ChangeEvent) => {
+    const newValue = (event.target as HTMLInputElement).value|| "";
+    onTextChange(newValue);
+  };
+
+  return (
+    <input
+      className="text-input"
+      type="text"
+      placeholder={placeholder}
+      value={value}
+      onChange={changeInputHandler}
+    />
+  );
+}
