b37abeadc9a6e4fe4d75abda11f542c9b0dd98ec
Mohammad <msharifi@cafebazaar.ir>
2020-04-18 19:53:23 +0430

    feat: use TextInput component in Screen

--- src/components/Screen/index.tsx
+++ src/components/Screen/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import TextInput from "../TextInput";
 
-import  "./index.scss";
+import "./index.scss";
 
 interface ScreenProps {
   isOpen: boolean;
@@ -8,10 +9,15 @@ interface ScreenProps {
 }
 
 export default function Screen({ isOpen, closeClickHandler }: ScreenProps) {
+  const [name, setName] = useState("");
   const screenShowingClass = isOpen ? "screen--open" : "";
   return (
     <div className={`screen ${screenShowingClass}`}>
-      <p>Hello Man How Are You</p>
+      <TextInput
+        placeholder="Enter Your Name"
+        value={name}
+        onTextChange={setName}
+      />
       <button className="screen__exit-button" onClick={closeClickHandler}>
         X
       </button>
