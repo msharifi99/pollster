29920d966d57adebedaa26db0221e0e190564d69
Mohammad <msharifi@cafebazaar.ir>
2020-04-24 19:20:38 +0430

    refactor: make screen dom better

--- src/components/Screen/index.tsx
+++ src/components/Screen/index.tsx
@@ -18,22 +18,24 @@ export default function Screen({ isOpen, closeClickHandler }: ScreenProps) {
     getPoll().then(({ inputs }) => {
       setPollInputs(inputs);
     });
-  },[setPollInputs]);
+    
 
   const screenShowingClass = isOpen ? "screen--open" : "";
   return (
     <div className={`screen ${screenShowingClass}`}>
-      {pollInputs.map((poll) => (
-        <TextInput
-          key={poll.id}
-          placeholder={poll.label}
-          value={name}
-          onTextChange={setName}
-        />
-      ))}
       <button className="screen__exit-button" onClick={closeClickHandler}>
         X
       </button>
+      <div>
+        {pollInputs.map((poll) => (
+          <TextInput
+            key={poll.id}
+            placeholder={poll.label}
+            value={name}
+            onTextChange={setName}
+          />
+        ))}
+      </div>
     </div>
   );
 }
